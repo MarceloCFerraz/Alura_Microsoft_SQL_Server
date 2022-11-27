b6ad8f9495c60398681f6f49b88b38de7f2217fe
Marcelo Carvalho Ferraz <carvalhoferrazmarcelo@gmail.com>
2022-11-27 10:56:16 -0300

    Added 'not null' constraint to CPF column in table Clientes; Inserted the first client in the database

--- Alterar Propriedades de Campos.sql
+++ Alterar Propriedades de Campos.sql
@@ -4,3 +4,12 @@ VARCHAR(10) NOT NULL
 
 ALTER TABLE [PRODUTOS]
 ADD CONSTRAINT PK_ID_PRODUTO PRIMARY KEY CLUSTERED ([CODIGO DO PRODUTO])
+
+/*	---------------------------------------------------------	*/
+
+alter table [clientes]
+alter column [CPF]
+varchar(11) not null
+
+alter table [clientes]
+add constraint PK_ID_CLIENTE primary key clustered ([CPF])
